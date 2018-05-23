1ac4c628bd61496680507b4b0db02eb21309d0df
Joel Serna Moreno <joelsernamoreno@gmail.com>
2018-05-24 01:01:01 +0200

    Update payload

--- atmega32u4_arduino_leonardo/fake_update_screen/fake_update_screen.ino
+++ atmega32u4_arduino_leonardo/fake_update_screen/fake_update_screen.ino
@@ -8,5 +8,9 @@ void setup() {
 void loop() {
   delay(5000);
   CommandAtRunBarMSWIN("iexplore -k http://fakeupdate.net/win10u/index.html");
+  delay(5000);
+  Keyboard.press(KEY_F11);
+  delay(100);
+  Keyboard.releaseAll();
   while(1);
 }
